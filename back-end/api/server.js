1d770461f4f0722d10509ed12fda2d5f806bbadc
Phelps <ultimaterafa79@gmail.com>
2025-02-15 10:36:22 -0300

    alteração no arquivo .env

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,21 +1,30 @@
 import express from "express";
 import { db } from "./connect.js";
 import cors from "cors";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3001;
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
     res.send("Página inicial da api")
 })
 
-app.get("/artists", async (req, res) => {
+app.get("/api/artists", async (req, res) => {
     res.send(await db.collection("artists").find({}).toArray());
 })
 
-app.get("/songs", async (req, res) => {
+app.get("/api/songs", async (req, res) => {
     res.send(await db.collection("songs").find({}).toArray());
+})
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("*", async (req, res) => {
+    res.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 })
 
 app.listen(PORT, () => {
