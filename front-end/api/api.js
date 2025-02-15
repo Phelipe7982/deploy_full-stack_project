9689a16c5b9566ed667418b62d056dbe7395e910
Phelps <ultimaterafa79@gmail.com>
2025-02-15 10:53:09 -0300

    ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import "dotenv/config";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+// const { NODE_ENV } = process.env;
+const URL = "https://deploy-full-stack-project.onrender.com/api";
 
 const resposeArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
